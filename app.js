2462d6761742cc2e073c3b25aa585bf44b779736
Andrés Reyes Romero <arromero0408@gmail.com>
2024-09-22 01:37:36 -0600

    Modified: Fixed slider backgrounds as requested

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-var nextBtn = document.querySelector('.next'),
-    prevBtn = document.querySelector('.prev'),
-    carousel = document.querySelector('.carousel'),
-    list = document.querySelector('.list'), 
-    item = document.querySelectorAll('.item'),
-    runningTime = document.querySelector('.carousel .timeRunning') 
+let nextBtn = document.querySelector('.next')
+let prevBtn = document.querySelector('.prev')
+let carousel = document.querySelector('.carousel')
+let list = document.querySelector('.list')
+let item = document.querySelectorAll('.item')
+runningTime = document.querySelector('.carousel .timeRunning')
 
-    document.querySelector('#menu-btn').onclick = () =>{
-        navbar.classList.toggle('active');
-     };
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+};
 
-let timeRunning = 2000 
+let timeRunning = 5000
 let timeAutoNext = 5000
 
-nextBtn.onclick = function(){
+nextBtn.onclick = function () {
     showSlider('next')
 }
 
-prevBtn.onclick = function(){
+prevBtn.onclick = function () {
     showSlider('prev')
 }
 
-let runTimeOut 
+let runTimeOut
 
 let runNextAuto = setTimeout(() => {
     nextBtn.click()
@@ -30,26 +30,38 @@ let runNextAuto = setTimeout(() => {
 function resetTimeAnimation() {
     runningTime.style.animation = 'none'
     runningTime.offsetHeight /* trigger reflow */
-    runningTime.style.animation = null 
+    runningTime.style.animation = null
     runningTime.style.animation = 'runningTime 4s linear 1 forwards'
 }
 
 
 function showSlider(type) {
     let sliderItemsDom = list.querySelectorAll('.carousel .list .item')
-    if(type === 'next'){
+
+    if (type === 'next') {
         list.appendChild(sliderItemsDom[0])
+            // Accede al contenido de texto del elemento con la clase 'name'
+            let oldName = sliderItemsDom[1].querySelector('.name');
+            let newName = sliderItemsDom[2].querySelector('.name');
+    
+            if (oldName && newName) {
+                // Reemplaza el contenido de texto del nuevo elemento con el del viejo
+                oldName.textContent = newName.textContent;
+            }
+        
         carousel.classList.add('next')
-    } else{
+    } else {
         list.prepend(sliderItemsDom[sliderItemsDom.length - 1])
         carousel.classList.add('prev')
+
     }
 
     clearTimeout(runTimeOut)
 
-    runTimeOut = setTimeout( () => {
+    runTimeOut = setTimeout(() => {
         carousel.classList.remove('next')
         carousel.classList.remove('prev')
+
     }, timeRunning)
 
 
@@ -63,23 +75,23 @@ function showSlider(type) {
 
 // Start the initial animation 
 
-// Start the initial manu hamburger
+// // Start the initial manu hamburger
 resetTimeAnimation()
 let menu = document.querySelector('#menu-btn');
 let navbarLinks = document.querySelector('.header .navbar .links');
 
-menu.onclick = () =>{
-   menu.classList.toggle('fa-times');
-   navbarLinks.classList.toggle('active');
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbarLinks.classList.toggle('active');
 }
 
-window.onscroll = () =>{
-   menu.classList.remove('fa-times');
-   navbarLinks.classList.remove('active');
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navbarLinks.classList.remove('active');
 
-   if(window.scrollY > 60){
-      document.querySelector('.header .navbar').classList.add('active');
-   }else{
-      document.querySelector('.header .navbar').classList.remove('active');
-   }
+    if (window.scrollY > 60) {
+        document.querySelector('.header .navbar').classList.add('active');
+    } else {
+        document.querySelector('.header .navbar').classList.remove('active');
+    }
 }
